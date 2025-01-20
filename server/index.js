85d9b0cf1c008dae9891bf04aa15cf0d2f08bf8f
bogdan.robu57@gmail.com <bogdan.robu57@gmail.com>
2025-01-20 16:28:33 +0200

    added dev script

--- server/index.js
+++ server/index.js
@@ -16,9 +16,6 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-// const CONNECTION_URL =
-//   "mongodb+srv://root:root@cluster0.91fyb.mongodb.net/test?retryWrites=true&w=majority";
-
 const PORT = process.env.PORT || 5000;
 
 mongoose
