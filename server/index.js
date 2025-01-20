266b5865019a37a79480945c5909595593c57f3d
bogdan.robu57@gmail.com <bogdan.robu57@gmail.com>
2025-01-20 16:40:30 +0200

    deploy fix

--- server/index.js
+++ server/index.js
@@ -19,15 +19,10 @@ app.use("/posts", postRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
     )
   )
   .catch((error) => console.log(`${error} did not connect`));
-
-mongoose.set("useFindAndModify", false);
